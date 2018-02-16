b2968efb46e4e5382367e835eaddf72350a622be
Eric Trombly <etrombly@yahoo.com>
2018-02-15 18:00:05 -0600

    add initial gcode parsing

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,10 @@ extern crate cortex_m_rtfm as rtfm;
 extern crate stm32f103xx_hal as hal;
 extern crate stepper_driver;
 extern crate nb;
+extern crate gcode;
+
+use gcode::{Tokenizer, Parser};
+use gcode::parser::{Line, CommandKind};
 
 //use core::fmt::Write;
 
@@ -23,8 +27,13 @@ use stepper_driver::ic::ULN2003;
 use hal::gpio::gpioa::{PA1, PA2, PA3, PA4};
 use hal::gpio::{Output, PushPull};
 
+const GCODE: &'static str = "G0 X10 Y0\nG0 X10 Y10\nG0 X0 Y10\nG0 X0 Y0";
+const STEPS_MM: f32 = 120.0;
+const STEP_SIZE: f32 = 1.0 / STEPS_MM;
+
+
 // CONNECTIONS
-type STEPPER1 = Stepper<
+type STEPPER_X = Stepper<
     PA1<Output<PushPull>>,
     PA2<Output<PushPull>>,
     PA3<Output<PushPull>>,
@@ -32,18 +41,24 @@ type STEPPER1 = Stepper<
     ULN2003,
 >;
 
+pub struct Move {
+    pub x: f32,
+    pub y: f32,
+}
+
 app! {
     device: stm32f103xx,
 
     resources: {
-        static STEPPER1: STEPPER1;
+        static STEPPER_X: STEPPER_X;
+        static CURRENT: Move;
     },
 
-    // Both SYS_TICK and TIM2 have access to the `COUNTER` data
+    // SYS_TICK handles stepper movement
     tasks: {
         SYS_TICK: {
             path: sys_tick,
-            resources: [STEPPER1],
+            resources: [STEPPER_X, CURRENT],
         },
     },
 }
@@ -57,12 +72,13 @@ fn init(mut p: init::Peripherals) -> init::LateResources {
 
     // configure the system timer to generate one interrupt every second
     p.core.SYST.set_clock_source(SystClkSource::Core);
-    p.core.SYST.set_reload(250_000); // 1/32s
+    p.core.SYST.set_reload(100_000); 
     p.core.SYST.enable_interrupt();
     p.core.SYST.enable_counter();
-    let stepper1 = Stepper::uln2003(Direction::CW, gpioa.pa1.into_push_pull_output(&mut gpioa.crl), gpioa.pa2.into_push_pull_output(&mut gpioa.crl), gpioa.pa3.into_push_pull_output(&mut gpioa.crl), gpioa.pa4.into_push_pull_output(&mut gpioa.crl));
+    let stepper_x = Stepper::uln2003(Direction::CW, gpioa.pa1.into_push_pull_output(&mut gpioa.crl), gpioa.pa2.into_push_pull_output(&mut gpioa.crl), gpioa.pa3.into_push_pull_output(&mut gpioa.crl), gpioa.pa4.into_push_pull_output(&mut gpioa.crl));
     init::LateResources {
-        STEPPER1: stepper1,
+        STEPPER_X: stepper_x,
+        CURRENT: Move{x: 0.0, y: 0.0},
     }
 }
 
@@ -81,5 +97,27 @@ fn idle() -> ! {
 // has one field per resource declared in `app!`.
 #[allow(unsafe_code)]
 fn sys_tick(_t: &mut Threshold, mut r: SYS_TICK::Resources) {
-    r.STEPPER1.step();
+    // add check if all current moves are done and grab next move from buffer
+    if r.CURRENT.x > 0.0 {
+        r.STEPPER_X.step();
+        r.CURRENT.x -= STEP_SIZE;
+    }
+    
+
+    // This section should be in the serial handling section
+    let lexer = Tokenizer::new(GCODE.chars());
+    let tokens = lexer.filter_map(|t| t.ok());
+    let parser = Parser::new(tokens);
+    for line in parser {
+        if let Line::Cmd(command) = line.unwrap() {
+            if (command.kind, command.number) == (CommandKind::G, gcode::parser::Number::Integer(0)) {
+                if let Some(x) = command.args.x {
+                    r.CURRENT.x = x;
+                }
+                if let Some(y) = command.args.y {
+                    r.CURRENT.y = y;
+                }
+            }
+        }
+    }
 }
