bdecf37955a933a531884f4c8382e946efdd677b
Eric Trombly <etrombly@yahoo.com>
2018-02-16 05:42:47 -0600

    add y stepper

--- src/main.rs
+++ src/main.rs
@@ -13,23 +13,27 @@ extern crate nb;
 extern crate gcode;
 
 use gcode::{Tokenizer, Parser};
-use gcode::parser::{Line, CommandKind};
+use gcode::parser::{Line, CommandKind, Number};
 
 use hal::prelude::*;
-use cortex_m::peripheral::syst::SystClkSource;
 use hal::stm32f103xx;
+use hal::gpio::gpioa::{PA1, PA2, PA3, PA4};
+use hal::gpio::gpiob::{PB4, PB5, PB6, PB7};
+use hal::gpio::{Output, PushPull};
+
+use cortex_m::peripheral::syst::SystClkSource;
+
 use rtfm::{app, Threshold};
+
 use stepper_driver::{Stepper, Direction};
 use stepper_driver::ic::ULN2003;
 
-use hal::gpio::gpioa::{PA1, PA2, PA3, PA4};
-use hal::gpio::{Output, PushPull};
-
 // TODO: have these initialized over the serial connection
 const GCODE: &'static str = "G0 X10 Y0\nG0 X10 Y10\nG0 X0 Y10\nG0 X0 Y0";
-const STEPS_MM: f32 = 120.0;
-const STEP_SIZE: f32 = 1.0 / STEPS_MM;
-
+const X_STEPS_MM: f32 = 120.0;
+const X_STEP_SIZE: f32 = 1.0 / X_STEPS_MM;
+const Y_STEPS_MM: f32 = 120.0;
+const Y_STEP_SIZE: f32 = 1.0 / Y_STEPS_MM;
 
 // CONNECTIONS
 type STEPPER_X = Stepper<
@@ -37,6 +41,14 @@ type STEPPER_X = Stepper<
     PA2<Output<PushPull>>,
     PA3<Output<PushPull>>,
     PA4<Output<PushPull>>,
+    ULN2003,
+>;
+
+type STEPPER_Y = Stepper<
+    PB4<Output<PushPull>>,
+    PB5<Output<PushPull>>,
+    PB6<Output<PushPull>>,
+    PB7<Output<PushPull>>,
     ULN2003,
 >;
 
@@ -94,17 +106,16 @@ fn sys_tick(_t: &mut Threshold, mut r: SYS_TICK::Resources) {
     // TODO: add check if all current moves are done and grab next move from buffer
     if r.CURRENT.x > 0.0 {
         r.STEPPER_X.step();
-        r.CURRENT.x -= STEP_SIZE;
+        r.CURRENT.x -= X_STEP_SIZE;
     }
     
-
     // TODO: This section should be in the serial handling section
     let lexer = Tokenizer::new(GCODE.chars());
     let tokens = lexer.filter_map(|t| t.ok());
     let parser = Parser::new(tokens);
     for line in parser {
         if let Line::Cmd(command) = line.unwrap() {
-            if (command.kind, command.number) == (CommandKind::G, gcode::parser::Number::Integer(0)) {
+            if (command.kind, command.number) == (CommandKind::G, Number::Integer(0)) {
                 // TODO: these should add the command to the buffer, rather than current move
                 if let Some(x) = command.args.x {
                     r.CURRENT.x = x;
