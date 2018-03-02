09955eb909d1015013f0b08f6204d525e2302bd5
Eric Trombly <etrombly@yahoo.com>
2018-03-01 20:32:50 -0600

    change x and y to seperate timers

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ extern crate cortex_m_rtfm as rtfm;
 extern crate either;
 extern crate gcode;
 extern crate nb;
+extern crate m;
 extern crate byteorder;
 extern crate cobs;
 extern crate stepper_driver;
@@ -18,6 +19,8 @@ extern crate stm32f103xx_hal as hal;
 use core::str;
 
 use byteorder::{ByteOrder, LE};
+
+use m::Float;
 
 use gcode::{Parser, Tokenizer};
 use gcode::parser::{CommandKind, Line, Number};
@@ -42,12 +45,20 @@ use rtfm::{app, Threshold};
 use stepper_driver::{Direction, Stepper};
 use stepper_driver::ic::ULN2003;
 
-// TODO: have these initialized over the serial connection
-const GCODE: &'static str = "G0 X10 Y0\nG0 X10 Y10\nG0 X0 Y10\nG0 X0 Y0";
-const X_STEPS_MM: f32 = 120.0;
+// TODO: have these initialized over the serial connection and save to flash
+const X_STEPS_MM: f32 = 600.0;
 const X_STEP_SIZE: f32 = 1.0 / X_STEPS_MM;
-const Y_STEPS_MM: f32 = 120.0;
+const Y_STEPS_MM: f32 = 600.0;
 const Y_STEP_SIZE: f32 = 1.0 / Y_STEPS_MM;
+// max velocity in mm/s
+const MAX_SPEED_X: f32 = 0.6;
+const MAX_SPEED_Y: f32 = 0.6;
+// min velocity
+const JERK_X: f32 = 0.3;
+const JERK_Y: f32 = 0.3;
+// acceleration
+const ACCEL_X: f32 = 1.0;
+const ACCEL_Y: f32 = 1.0;
 
 const TX_SZ: usize = 18;
 const RX_SZ: usize = 20;
@@ -77,7 +88,18 @@ type STEPPER_Y = Stepper<
     ULN2003,
 >;
 
-const G0: (CommandKind, Number) = (CommandKind::G, Number::Integer(0));
+const G0: (CommandKind, Number) = (CommandKind::G, Number::Integer(0)); // Move
+const M0: (CommandKind, Number) = (CommandKind::M, Number::Integer(0)); // unconditional stop (unimplemented)
+const M18: (CommandKind, Number) = (CommandKind::M, Number::Integer(18)); // disable steppers (unimplemented)
+const M92: (CommandKind, Number) = (CommandKind::M, Number::Integer(92)); // Set steps per mm (unimplemented)
+const M114: (CommandKind, Number) = (CommandKind::M, Number::Integer(114)); // get current position (unimplemented)
+const M202: (CommandKind, Number) = (CommandKind::M, Number::Integer(202)); // Set max travel accel (unimplemented)
+const M204: (CommandKind, Number) = (CommandKind::M, Number::Integer(204)); // Set default accel (unimplemented)
+const M205: (CommandKind, Number) = (CommandKind::M, Number::Integer(205)); // Advanced settings / jerk (unimplemented)
+const M222: (CommandKind, Number) = (CommandKind::M, Number::Integer(222)); // Set speed for fast XY Moves (unimplemented)
+const M223: (CommandKind, Number) = (CommandKind::M, Number::Integer(223)); // Set speed for fast Z Moves (unimplemented)
+const M500: (CommandKind, Number) = (CommandKind::M, Number::Integer(500)); // Save settings (unimplemented)
+const M501: (CommandKind, Number) = (CommandKind::M, Number::Integer(501)); // Restore settings (unimplemented)
 
 pub struct Move {
     pub x: f32,
@@ -91,7 +113,8 @@ app! {
         static STEPPER_X: STEPPER_X;
         static STEPPER_Y: STEPPER_Y;
         static CURRENT: Move;
-        static TIMER: Timer<stm32f103xx::TIM2>;
+        static TIMER_X: Timer<stm32f103xx::TIM2>;
+        static TIMER_Y: Timer<stm32f103xx::TIM3>;
         static TX: Option<Either<(TX_BUF, dma1::C4, TX), Transfer<R, TX_BUF, dma1::C4, TX>>> = None;
         static RX_BUF: [u8; RX_SZ] = [0; RX_SZ];
         static RX: Option<Either<(RX_BUF, dma1::C5, RX), Transfer<W, RX_BUF, dma1::C5, RX>>> = None;
@@ -102,16 +125,20 @@ app! {
         resources: [RX_BUF, TX_BUF],
     },
 
-    // Timer 2 handles stepper movement
     tasks: {
         SYS_TICK: {
             path: sys_tick,
-            resources: [TIMER, CURRENT],
+            resources: [TIMER_X, TIMER_Y, CURRENT],
         },
 
         TIM2: {
-            path: timer2,
-            resources: [STEPPER_X, STEPPER_Y, CURRENT, TIMER],
+            path: timer_x,
+            resources: [STEPPER_X, CURRENT, TIMER_X],
+        },
+
+        TIM3: {
+            path: timer_y,
+            resources: [STEPPER_Y, CURRENT, TIMER_Y],
         },
 
         DMA1_CHANNEL5: {
@@ -132,9 +159,11 @@ fn init(mut p: init::Peripherals, r: init::Resources) -> init::LateResources {
         .pclk1(32.mhz())
         .freeze(&mut flash.acr);
     
-    
-    let mut tim2 = Timer::tim2(p.device.TIM2, 180.hz(), clocks, &mut rcc.apb1);
+    // TODO: start at 0 and calculate velocity in sys_tick
+    let mut tim2 = Timer::tim2(p.device.TIM2, ((MAX_SPEED_X / X_STEP_SIZE) as u32).hz(), clocks, &mut rcc.apb1);
     tim2.listen(Event::Update);
+    let mut tim3 = Timer::tim3(p.device.TIM3, ((MAX_SPEED_Y / Y_STEP_SIZE) as u32).hz(), clocks, &mut rcc.apb1);
+    tim3.listen(Event::Update);
 
     let mut afio = p.device.AFIO.constrain(&mut rcc.apb2);
 
@@ -191,7 +220,8 @@ fn init(mut p: init::Peripherals, r: init::Resources) -> init::LateResources {
         STEPPER_X: stepper_x,
         STEPPER_Y: stepper_y,
         CURRENT: Move { x: 0.0, y: 0.0 },
-        TIMER: tim2,
+        TIMER_X: tim2,
+        TIMER_Y: tim3,
     }
 }
 
@@ -206,10 +236,14 @@ fn sys_tick(_t: &mut Threshold, r: SYS_TICK::Resources) {
     // TODO: Send heartbeat and any other responses back through serial
 
     //TODO: calculate timer speeds based on accel and jerk settings
+    //r.X_TIMER.start((X_SPEED / X_STEP_SIZE).hz());
+
+    let distance = 10.0; //temp value for velocity calc, replace with r.CURRENT.x or whatever
+    let velocity_max_x = ((ACCEL_X * JERK_X * JERK_X) + (2.0 * (ACCEL_X * ACCEL_X * distance)) / (ACCEL_X * 2.0)).sqrt();
 }
 
 // This is the task handler of the TIM2 exception
-fn timer2(_t: &mut Threshold, mut r: TIM2::Resources) {
+fn timer_x(_t: &mut Threshold, mut r: TIM2::Resources) {
     // TODO: add check if all current moves are done and grab next move from buffer
     // TODO: refactor this to remove duplication
     if r.CURRENT.x - X_STEP_SIZE > 0.0 || r.CURRENT.x + X_STEP_SIZE < 0.0{
@@ -226,7 +260,13 @@ fn timer2(_t: &mut Threshold, mut r: TIM2::Resources) {
         r.STEPPER_X.disable();
         r.CURRENT.x = 0.0;
     }
+    r.TIMER_X.wait().unwrap();
+}
 
+// This is the task handler of the TIM2 exception
+fn timer_y(_t: &mut Threshold, mut r: TIM3::Resources) {
+    // TODO: add check if all current moves are done and grab next move from buffer
+    // TODO: refactor this to remove duplication
     if r.CURRENT.y - Y_STEP_SIZE > 0.0 || r.CURRENT.y + Y_STEP_SIZE < 0.0{
         if r.CURRENT.y > 0.0 {
             r.STEPPER_Y.direction(Direction::CW);
@@ -241,7 +281,7 @@ fn timer2(_t: &mut Threshold, mut r: TIM2::Resources) {
         r.STEPPER_Y.disable();
         r.CURRENT.y = 0.0;
     }
-    r.TIMER.wait().unwrap();
+    r.TIMER_Y.wait().unwrap();
 }
 
 fn rx(_t: &mut Threshold, mut r: DMA1_CHANNEL5::Resources) {
@@ -272,6 +312,7 @@ fn rx(_t: &mut Threshold, mut r: DMA1_CHANNEL5::Resources) {
 
     *r.RX = Some(Either::Right(rx.read_exact(c, buf)));
 
+    // TODO: take this out later
     let (buf, c, tx) = match r.TX.take().unwrap() {
         Either::Left((buf, c, tx)) => (buf, c, tx),
         Either::Right(transfer) => transfer.wait(),
